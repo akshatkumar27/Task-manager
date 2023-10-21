3d7ddb5ff225bcbfd175ddf76f4a439e8a33d3e6
hrutika985 <hrutika567@gmail.com>
2023-10-21 17:24:22 +0530

    Test auth routes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,33 +1,32 @@
-const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User.model");
 
 async function register(req, res) {
 	try {
-		const username = req.body.Username;
-		const email = req.body.Email;
+		const username = req.body.username;
+		const email = req.body.email;
 		// Check if user already exists
 		const user = await User.findOne({ Email: email });
 		if (user) {
 			return res.status(400).json({ message: "User already exists" });
 		}
-		if (req.body.Username.length == 0) {
+		if (req.body.username.length == 0) {
 			return res.status(400).json({ message: "Username is empty" });
 		}
-		if (req.body.Email.length == 0) {
+		if (req.body.email.length == 0) {
 			return res.status(400).json({ message: "Email is empty" });
 		}
-		if (req.body.Password.length == 0) {
+		if (req.body.password.length == 0) {
 			return res.status(400).json({ message: "Password is empty" });
 		}
 		// Hash password
 		const salt = await bcrypt.genSalt(10);
-		const hashedPassword = await bcrypt.hash(req.body.Password, salt);
-		console.log(username, email, req.body.Password, hashedPassword);
+		const hashedPassword = await bcrypt.hash(req.body.password, salt);
+		console.log(username, email, req.body.password, hashedPassword);
 		// Create new user
 		const newUser = new User({
-			Username: req.body.Username,
+			Username: req.body.username,
 			Email: email,
 			Password: hashedPassword,
 		});
@@ -41,12 +40,21 @@ async function register(req, res) {
 
 async function login(req, res) {
 	try {
-		const email = req.body.Email;
-		const password = req.body.Password;
+		const email = req.body.email;
+		const password = req.body.password;
 		// Check if user already exists
 		const user = await User.findOne({ Email: email });
 		if (user) {
+			console.log(`User object retrieved: ${user}`);
+			console.log(`Password: ${password}`);
+			console.log(`User Password: ${user.Password}`);
 			bcrypt.compare(password, user.Password, (err, result) => {
+				if (err) {
+					console.error(err);
+					return res.json({
+						message: "An error occurred during password comparison",
+					});
+				}
 				if (!result) {
 					res.json({
 						message: "Invalid Password",
